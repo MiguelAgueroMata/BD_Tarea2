f0f5810b9b33a3e43235e98d167adec211abe9a6
Miguel Aguero Mata <miguel.eduardo17197@gmail.com>
2024-10-03 21:14:28 -0600

    Boton listar/insertar empleados

---
+++ tarea_2/src/app/webPage/components/ListEmployees.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ListEmployees: React.FC = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Lista de Empleados</h2>
+      {/* Aquí puedes agregar la lógica para listar los empleados */}
+    </div>
+  );
+};
+
+export default ListEmployees;
